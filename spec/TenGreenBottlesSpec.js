98537f26162e84faa04840cb5aa8ccccff6cf445
Daniel <dct.lau17@gmail.com>
2018-10-02 15:35:27 +0100

    Refactor code and introduce OOP

--- spec/TenGreenBottlesSpec.js
+++ spec/TenGreenBottlesSpec.js
@@ -1,8 +1,20 @@
 describe('tenGreenBottles', function(){
+  var song
+  beforeEach(function (){
+    song = new TenGreenBottles(10)
+  })
+
   it('returns 10 Green bottles lyrics', function(){
-    expect(tenGreenBottles(10)).toEqual('Ten green bottles sitting on the wall,\n'+
+    expect(song.sing(10)).toEqual('Ten green bottles sitting on the wall,\n'+
                                        'Ten green bottles sitting on the wall,\n'+
                                        'And if one green bottle should accidentally fall,\n'+
                                        `There’ll be nine green bottles sitting on the wall.`)
   })
+
+  // it('lyrics for 1 bottle', function(){
+  //   expect(song.sing(1)).toEqual('One green bottle sitting on the wall,\n'+
+  //                                      'One green bottle sitting on the wall,\n'+
+  //                                      'And if one green bottle should accidentally fall,\n'+
+  //                                      `There’ll be zero green bottles sitting on the wall.`)
+  // })
 })
--- src/TenGreenBottles.js
+++ src/TenGreenBottles.js
@@ -1,11 +1,28 @@
-function tenGreenBottles(num){
-  var firstSecondLine = `${numberToWords.toWords(num).capitalize()} green bottles sitting on the wall,\n`
-  var thirdLine = 'And if one green bottle should accidentally fall,\n'
-  var lastLine = `There’ll be ${numberToWords.toWords(num - 1)} green bottles sitting on the wall.`
+(function(exports){
+  function TenGreenBottles(num){
+    this.num = num
+  }
 
-  return firstSecondLine + firstSecondLine + thirdLine + lastLine;
-}
+  TenGreenBottles.prototype.sing = function(num){
+    var firstSecondLine = `${numberToWords.toWords(num).capitalize()} green bottles sitting on the wall,\n`
+    var thirdLine = 'And if one green bottle should accidentally fall,\n'
+    var lastLine = `There’ll be ${numberToWords.toWords(num - 1)} green bottles sitting on the wall.`
 
-String.prototype.capitalize = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
-}
+    return firstSecondLine + firstSecondLine + thirdLine + lastLine;
+  }
+
+  // function _last_verse(num){
+  //   var firstSecondLine = `${numberToWords.toWords(num).capitalize()} green bottles sitting on the wall,\n`
+  //   var thirdLine = 'And if one green bottle should accidentally fall,\n'
+  //   var lastLine = `There’ll be ${numberToWords.toWords(num - 1)} green bottles sitting on the wall.`
+  //
+  //   return firstSecondLine + firstSecondLine + thirdLine + lastLine;
+  // }
+
+  String.prototype.capitalize = function() {
+      return this.charAt(0).toUpperCase() + this.slice(1);
+  }
+
+  exports.TenGreenBottles = TenGreenBottles;
+
+})(this)
