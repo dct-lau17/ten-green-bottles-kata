98ad03ad93052d21b1bcf702aac8c6ec5e45bdd0
Daniel <dct.lau17@gmail.com>
2018-10-02 14:57:52 +0100

    Add num arguement to function

--- src/TenGreenBottles.js
+++ src/TenGreenBottles.js
@@ -1,7 +1,11 @@
-function tenGreenBottles(){
-  var firstSecondLine = 'Ten green bottles sitting on the wall,\n'
+function tenGreenBottles(num){
+  var firstSecondLine = `${numberToWords.toWords(num).capitalize()} green bottles sitting on the wall,\n`
   var thirdLine = 'And if one green bottle should accidentally fall,\n'
-  var lastLine = `There’ll be nine green bottles sitting on the wall.`
+  var lastLine = `There’ll be ${numberToWords.toWords(num - 1)} green bottles sitting on the wall.`
 
   return firstSecondLine + firstSecondLine + thirdLine + lastLine;
+}
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
 }
