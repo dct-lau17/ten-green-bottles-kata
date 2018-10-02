cf6cfb05a3bba3d8d09cc39701dc60b401b6b671
Daniel <dct.lau17@gmail.com>
2018-10-02 14:47:06 +0100

    Add first verse to tenGreenBottles function

---
+++ spec/TenGreenBottlesSpec.js
@@ -0,0 +1,8 @@
+describe('tenGreenBottles', function(){
+  it('returns 10 Green bottles lyrics', function(){
+    expect(tenGreenBottles()).toEqual('Ten green bottles sitting on the wall,\n'+
+                                       'Ten green bottles sitting on the wall,\n'+
+                                       'And if one green bottle should accidentally fall,\n'+
+                                       `There’ll be nine green bottles sitting on the wall.`)
+  })
+})
---
+++ src/TenGreenBottles.js
@@ -0,0 +1,7 @@
+function tenGreenBottles(){
+  var firstSecondLine = 'Ten green bottles sitting on the wall,\n'
+  var thirdLine = 'And if one green bottle should accidentally fall,\n'
+  var lastLine = `There’ll be nine green bottles sitting on the wall.`
+
+  return firstSecondLine + firstSecondLine + thirdLine + lastLine;
+}
